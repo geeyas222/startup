e02c5af0fd279456fc9387294fa61bc695aa4277
geeyas222 <py13671@nitaustralia.edu.au>
2023-11-11 22:15:37 +0800

    stars bg

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ const App = () => {
 
   return (
     <BrowserRouter>
+
       <div className='bg-primary w-full overflow-hidden relative'>
         <div className={` ${styles.paddingX} ${styles.flexCenter}  fixed top-0 left-0 w-full p-1 z-50 ${scrolled ? 'bg-primary shadow-lg rounded-full bg-opacity-60' : ''}`}>
           <div className={`${styles.boxWidth}`}>
@@ -34,29 +35,38 @@ const App = () => {
 
         <div className={`bg-primary ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-            <Hero /> {/*Hero section with slogan and image on side  */}
+            <div className="relative z-0">
+              <StarsCanvas />
+              <Hero /> {/*Hero section with slogan and image on side  */}
+            </div>
           </div>
         </div>
 
         <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
-
-            <Stats /> {/*Stats with counter  */}
-            <Billing /> {/*responsice web app building service */}
-            <CardDeal /> {/*Tech Support service*/}
-            <Business /> {/*Design webapp for business service*/}
-            <Project /> {/*projects*/}
-            {/* <StarsCanvas className="relative z-100"/> */}
-            <Price />
-            <CTA /> {/*contact us Form*/}
-            <Aboutus /> {/*About us Section*/}
-            <Footer /> {/*Footer section*/}
-
+            <div className="relative z-0">
+              <StarsCanvas />
+              <Stats /> {/*Stats with counter  */}
+              <Billing /> {/*responsice web app building service */}
+              <CardDeal /> {/*Tech Support service*/}
+              <Business /> {/*Design webapp for business service*/}
+              <Project /> {/*projects*/}
+              <div className="relative z-0">
+                <StarsCanvas />
+                <CTA /> {/*contact us Form*/}
+              </div>
+              <div className="relative z-0">
+                <StarsCanvas />
+                <Aboutus /> {/*About us Section*/}
+              </div>
+              <Footer /> {/*Footer section*/}
+            </div>
           </div>
         </div>
         {/* <Testomonials /> */}
         {/* <Client /> */}
       </div>
+
     </BrowserRouter>
   )
 }
