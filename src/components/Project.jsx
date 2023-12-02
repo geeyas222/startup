dd49d49f7f18a2de70fe1b1f8a890c23e460e337
geeyas222 <py13671@nitaustralia.edu.au>
2023-12-02 08:41:31 +0800

    mobile resp

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -85,9 +85,11 @@ const Project = () => {
                 <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
             </motion.div> */}
 
-            <div className='flex'>
+            <div className='flex flex-wrap overflow-auto'>
                 {projects.map((project, index) => (
-                    <Projects key={`project-${index}`} index={index} {...project} />
+                    <div key={`project-${index}`} className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 p-2'>
+                        <Projects {...project} />
+                    </div>
                 ))}
             </div>
         </>
